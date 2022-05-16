1ca9243f9f5963581416da2a086529d4bc7ddaf8
Vipul Kumar <vipulkumar13579@gmail.com>
2022-05-16 16:52:05 +0530

    abc

--- js/script.js
+++ js/script.js
@@ -3,17 +3,38 @@ new fullpage("#fullpage", {
   autoScrolling: true,
   scrollHorizontally: true,
   navigation: true,
-  scrollingSpeed: 700,
+  scrollingSpeed: 1000,
+  anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage"],
 
   onLeave: function (index, nextIndex, direction) {
     if (nextIndex.index == 1) {
-      console.log("vipul");
       anime({
         targets: ".js-letter",
         translateX: ["-105%", 0],
         easing: "easeInOutCubic",
-        duration: 800,
-        delay: anime.stagger(50, { start: 500 }),
+        duration: 900,
+        delay: anime.stagger(50, { start: 300 }),
+      });
+
+      anime({
+        targets: ".image__cover",
+        translateX: [0, "110%"],
+        translateZ: 0,
+        easing: "easeInOutQuart",
+        function(el, i) {
+          return 1200 - 200 * i;
+        },
+        delay: anime.stagger(50),
+      });
+
+      anime({
+        targets: ".image",
+        scale: [0.75, 1],
+        translateX: ["10%", 0],
+        translateZ: 0,
+        easing: "easeOutCubic",
+        duration: 1500,
+        delay: 50,
       });
     }
   },
