7413d933ba75cdff6e2bfa4f0b14bb8b6b7c8c6f
Vipul Kumar <vipulkumar13579@gmail.com>
2022-05-20 18:14:41 +0530

    abc

--- js/script.js
+++ js/script.js
@@ -6,45 +6,82 @@ new fullpage("#fullpage", {
   navigationPosition: "left",
   fixedElements: ".background",
   scrollingSpeed: 1300,
-  easingcss3: "cubic-bezier(.70,0,.30,1)",
+  easingcss3: "cubic-bezier(.60,0,.30,1)",
   anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage"],
   credits: { enabled: false },
 
   onLeave: function (index, nextIndex, direction) {
     if (nextIndex.index == 1) {
       anime({
-        targets: ".js-letter",
+        targets: ".s2 .js-letter",
         translateX: ["-105%", 0],
         easing: "easeInOutCubic",
         duration: 900,
-        delay: anime.stagger(50, { start: 500 }),
+        delay: anime.stagger(80, { start: 500 }),
       });
 
       anime({
-        targets: ".image__cover",
+        targets: ".s2 .image__cover",
         translateX: [0, "110%"],
         translateZ: 0,
         easing: "easeInOutQuart",
-        duration: 1200,
-        delay: anime.stagger(50, { start: 200 }),
+        duration: 1400,
+        delay: anime.stagger(100, { start: 200 }),
       });
 
       anime({
-        targets: ".image",
+        targets: ".s2 .image",
         scale: [0.75, 1],
         translateX: ["10%", 0],
         translateZ: 0,
         easing: "easeOutCubic",
-        duration: 2200,
+        duration: 2000,
         delay: 50,
       });
 
       anime({
-        targets: ".page-num p",
+        targets: ".s2 .page-num p",
         translateY: ["100%", 0],
         translateZ: 0,
         easing: "easeInOutCubic",
-        delay: 600,
+        delay: 1000,
+      });
+    }
+
+    if (nextIndex.index == 2) {
+      anime({
+        targets: ".s3 .js-letter",
+        translateX: ["-105%", 0],
+        easing: "easeInOutCubic",
+        duration: 900,
+        delay: anime.stagger(80, { start: 500 }),
+      });
+
+      anime({
+        targets: ".s3 .image__cover",
+        translateX: [0, "110%"],
+        translateZ: 0,
+        easing: "easeInOutQuart",
+        duration: 1400,
+        delay: anime.stagger(100, { start: 200 }),
+      });
+
+      anime({
+        targets: ".s3 .image",
+        scale: [0.75, 1],
+        translateX: ["10%", 0],
+        translateZ: 0,
+        easing: "easeOutCubic",
+        duration: 2000,
+        delay: 50,
+      });
+
+      anime({
+        targets: ".s3 .page-num p",
+        translateY: ["100%", 0],
+        translateZ: 0,
+        easing: "easeInOutCubic",
+        delay: 1000,
       });
     }
   },
