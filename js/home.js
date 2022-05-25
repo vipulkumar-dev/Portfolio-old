7993b8d6b4c5a065f69b09167b61b7bec6ad34c4
Vipul Kumar <vipulkumar13579@gmail.com>
2022-05-25 09:32:32 +0530

    abc

--- js/home.js
+++ js/home.js
@@ -9,18 +9,69 @@ const anim = lottie.loadAnimation({
   path: "../bodymovin/data.json", // the path to the animation json
 });
 
+const up = document.querySelectorAll(".up");
+
 anim.addEventListener("complete", function () {
   setTimeout(() => {
-    introContainer.style.transform = "translate(0%, -100%)";
+    introContainer.style.transform = "translate(0%, -120%)";
+
+    up.forEach((element) => {
+      element.classList.toggle("active");
+    });
+    // gsap.fromTo(
+    //   ".up",
+    //   { y: 500 },
+    //   {
+    //     duration: 1.5,
+    //     y: 0,
+    //     delay: 0.4,
+    //     ease: CustomEase.create("custom", "M0,0,C0.702,0,0,1,1,1"),
+    //   }
+    // );
+  }, 300);
+});
+
+const menuButton = document.querySelector(".menu");
+const roundBG = document.querySelector(".roundBG");
+const ul = document.querySelector(".nav__container .nav__bar .menu ul");
+
+let menuIstrue = true;
+
+menuButton.addEventListener("click", () => {
+  console.log("!");
+  menuButton.classList.toggle("active");
+  roundBG.classList.toggle("active");
+  ul.classList.toggle("active");
+
+  if (menuIstrue == true) {
     gsap.fromTo(
-      ".text",
-      { y: 500 },
+      ".nav__link",
+      { x: 300, opacity: 0 },
       {
+        x: 0,
         opacity: 1,
-        y: 0,
-        delay: 1,
-        ease: "power4.out",
+        stagger: 0.04,
+        delay: 0.4,
       }
     );
-  }, 300);
+
+    menuIstrue = false;
+  } else {
+    gsap.fromTo(
+      ".nav__link",
+      {
+        x: 0,
+        opacity: 1,
+      },
+      {
+        x: 300,
+        duration: 0,
+        opacity: 0,
+        stagger: 0.07,
+        delay: 0.4,
+      }
+    );
+
+    menuIstrue = true;
+  }
 });
